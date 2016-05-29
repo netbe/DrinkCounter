bf4d9401545d853ed200c35f0492b4429f830bf3
François Benaiteau <francois.benaiteau@gmail.com>
2016-05-29 14:09:36 -0500

    Initial Commit

---
+++ DrinkCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrinkCounter
+//
+//  Created by François Benaiteau on 29/05/16.
+//  Copyright © 2016 François Benaiteau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
